355402089c7d5673f990122c63d20e727b4e1107
huydothe <huydothe1999@gmail.com>
2023-08-15 11:46:57 +0700

    update: new btn

--- menu-effect/script.js
+++ menu-effect/script.js
@@ -1,13 +1,14 @@
 const data = ["Home", "About", "Portfolio", "Contact", "Other"];
 const menu = document.getElementById("menu");
 
-data.forEach((item) => {
+data.forEach((item, index) => {
   const r = document.createElement("div");
   r.classList.add("wrapper");
   const button = document.createElement("div");
   const division = document.createElement("div");
   const text = document.createElement("div");
   text.classList.add("text");
+  text.setAttribute("data-text", item);
   r.appendChild(button);
   button.appendChild(division);
   button.appendChild(text);
